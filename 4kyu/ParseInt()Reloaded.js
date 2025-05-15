61a8317ce06f1a7c2d03fc74dce0e77669002d1d
JPKawasaki <joshuapascallambertz@gmail.com>
2025-05-15 13:37:30 +0200

    Added new Challenges

---
+++ 4kyu/ParseInt()Reloaded.js
@@ -0,0 +1,66 @@
+/* In this kata we want to convert a string into an integer.
+The strings simply represent the numbers in words.
+
+Examples:
+    "one" => 1
+    "twenty" => 20
+    "two hundred forty-six" => 246
+    "seven hundred eighty-three thousand nine hundred and nineteen" => 783919
+
+Additional Notes:
+    The minimum number is "zero" (inclusively)
+    The maximum number, which must be supported is 1 million (inclusively)
+    The "and" in e.g. "one hundred and twenty-four" is optional, in some cases it's present and in others it's not
+    All tested numbers are valid, you don't need to validate them*/
+
+//My Solution
+function parseInt(string) {
+  const nums = {
+    "zero": 0, "one": 1, "two": 2, "three": 3, "four": 4,
+    "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9,
+    "ten": 10, "eleven": 11, "twelve": 12, "thirteen": 13,
+    "fourteen": 14, "fifteen": 15, "sixteen": 16,
+    "seventeen": 17, "eighteen": 18, "nineteen": 19,
+    "twenty": 20, "thirty": 30, "forty": 40, "fifty": 50,
+    "sixty": 60, "seventy": 70, "eighty": 80, "ninety": 90,
+    "hundred": 100, "thousand": 1000, "million": 1000000
+  };
+
+  const words = string.split(/[\s-]+/);
+  let total = 0;
+  let current = 0;
+
+  for (let word of words) {
+    if (!(word in nums)) continue;
+
+    let value = nums[word];
+
+    if (value < 100) {
+      current += value;
+    } else if (value === 100) {
+      current *= 100;
+    } else {
+      current *= value;
+      total += current;
+      current = 0;
+    }
+  }
+
+  return total + current;
+}
+
+//Best Practise
+const words = {
+  "zero":0, "one":1, "two":2, "three":3, "four":4, "five":5, "six":6, "seven":7, "eight":8, "nine":9, 
+  "ten":10, "eleven":11, "twelve":12, "thirteen":13, "fourteen":14, "fifteen":15, "sixteen":16, 
+  "seventeen":17, "eighteen":18, "nineteen":19, "twenty":20, "thirty":30, "forty":40, "fifty":50, 
+  "sixty":60, "seventy":70, "eighty":80, "ninety":90
+};
+const mult = { "hundred":100, "thousand":1000, "million": 1000000 };
+function parseInt(str) {
+  return str.split(/ |-/).reduce(function(value, word) {
+    if (words[word]) value += words[word];
+    if (mult[word]) value += mult[word] * (value % mult[word]) - (value % mult[word]);
+    return value;
+  },0);
+}
